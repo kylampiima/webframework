503f8d2dc5dde0df6ddc94b1d6f4bd07dcd84816
Kyla <mpiimakyla@gmail.com>
2023-10-17 15:32:22 +0100

    fourth push

---
+++ app_server/models/summary.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const summarySchema = new mongoose.Schema({
+    name: String,
+    author:{ type: String,
+            required:true
+        },
+    pages:{ 
+        type: Number,
+        required:true
+    },
+    genre:{ 
+        type: String,
+        required:true
+    },
+    published:{ 
+        type: Number,
+        required:true
+    },
+    description: { 
+        type: String,
+        required:true
+    } 
+    });
+    mongoose.model('Summary', summarySchema);
+    
